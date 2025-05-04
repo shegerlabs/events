c78aa5809d22f3eade33c364505b56b897e6fbea
Binalfew Kassa <binalfew@yahoo.com>
2025-05-04 18:56:48 +0300

    add shadcn to apps

--- apps/events/vite.config.ts
+++ apps/events/vite.config.ts
@@ -1,9 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 export default defineConfig(({ isSsrBuild }) => ({
 	build: {
 		rollupOptions: isSsrBuild
@@ -13,6 +13,11 @@ export default defineConfig(({ isSsrBuild }) => ({
 			: undefined,
 	},
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, './app'),
+		},
+	},
 	server: {
 		hmr: {
 			port: 24679,
